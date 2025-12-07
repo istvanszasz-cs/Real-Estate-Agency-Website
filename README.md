83de046af89a6382b9c4250f097ba7be0ccadff1
szaszisti05 <istiszasz2005@gmail.com>
2025-12-07 23:42:22 +0200

    Lab4 első próba

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Webprogramozás laborfeladatok
+A szerver elindítása után a 'localhost:<port>/' beírásával a böngészőbe, a weboldal elindul.
+MongoDB Atlast használ, és a database user adatok bele vannak hardcode-olva, úgyhogy elméletileg,
+kellene működjön a tesztelőnél is. ;) 
--- server.js
+++ server.js
@@ -3,28 +3,30 @@ import morgan from 'morgan';
 import fs from 'fs';
 import multer from 'multer';
 import path from 'path';
-import Datastore from 'nedb-promises';
+import { connectToDatabase, insertIntoDatabase, executeQuery, updateDatabase, getUsers } from './dal.js';
+import sanitize from 'mongo-sanitize';
+import { ObjectId } from 'mongodb';
 
-const db = Datastore.create({
-  filename: 'lakasok.db',
-  autoload: true,
-});
-
-let id = 1;
 const app = express();
 app.use(express.static('.'));
 app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
+app.use('/public', express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 const uploadDir = './uploads';
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
 }
+
 async function init() {
-  const lakasok = await db.find({}).sort({ id: 1 });
-  if (lakasok.length > 0) {
-    id = lakasok[lakasok.length - 1].id + 1;
+  try {
+    await connectToDatabase();
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
   }
 }
 const multerUpload = multer({
@@ -34,111 +36,140 @@ const multerUpload = multer({
   },
 });
 
+app.get('/', async (request, response) => {
+  try {
+    const talalatok = await executeQuery({});
+    response.render('fooldal', { talalatok });
+  } catch (err) {
+    console.error(`ERROR NUM: 500 ${err}`);
+    response.render('fooldal', { talalatok: [], hiba: 'Hiba a lakások lekérdezése során.' });
+  }
+});
+
 app.post('/kep_feltoltes', multerUpload.single('file'), async (request, response) => {
   if (!request.file) {
-    response.set('Content-Type', 'text/plain; charset=utf-8');
-    response.status(400).send('Hiba: Nincs feltöltött fájl!');
+    console.error(`ERROR NUM: 400`);
+    const lakas = null;
+    response.render('reszletes', { hiba: 'Hiba: Nincs fájl kiválasztva a feltöltéshez!', lakas });
     return;
   }
 
-  const lakasId = Number(request.body.id);
-  const lakas = await db.findOne({ id: lakasId });
-  if (!lakas) {
-    response.set('Content-Type', 'text/plain; charset=utf-8');
-    response.status(400).send('Hiba: Nincs ilyen ID-jú lakás az adatbázisban!');
+  const lakasId = sanitize(request.body.id);
+
+  const lakasArray = await executeQuery({ _id: new ObjectId(lakasId) });
+  if (lakasArray.length === 0) {
+    console.error(`ERROR NUM: 400`);
+    response.render('reszletes', { hiba: 'Hiba: Nem található a keresett ID az adatbázisban!' });
     return;
   }
+  const lakas = lakasArray[0];
 
   const kiterjesztes = path.extname(request.file.originalname);
   if (!['.jpg', '.jpeg', '.png', '.gif'].includes(kiterjesztes.toLowerCase())) {
     fs.unlinkSync(request.file.path);
-    response.set('Content-Type', 'text/plain; charset=utf-8');
-    response.status(400).send('Hiba: Érvénytelen fájltípus! Csak képfájlok engedélyezettek.');
+    console.error(`ERROR NUM: 400`);
+    response.render('reszletes', { hiba: 'Hiba: Érvénytelen fájltípus! Csak képfájlok engedélyezettek.' });
     return;
   }
-  const ujEleresiUt = `${uploadDir}/lakas_${lakasId}_img${kiterjesztes}`;
+  const ujEleresiUt = `./uploads/lakas_${lakasId}_img${kiterjesztes}`;
   fs.renameSync(request.file.path, ujEleresiUt);
 
   lakas.kepURL = ujEleresiUt;
-  await db.update({ id: lakasId }, { $set: { kepURL: ujEleresiUt } });
+  await updateDatabase({ _id: new ObjectId(lakasId) }, { kepURL: ujEleresiUt });
+  const talalatok = await executeQuery({});
+  response.render('fooldal', { talalatok });
+});
 
-  response.set('Content-Type', 'text/plain; charset=utf-8');
-  response.send(`Sikeres kép feltöltés! Lakás ID: ${lakasId}, Kép elérési út: ${ujEleresiUt}`);
+app.get('/letrehozas', async (request, response) => {
+  const felhasznalok = await getUsers();
+  response.render('ujlakas', { felhasznalok });
 });
 
 app.post('/letrehozas', async (request, response) => {
   const ujLakas = {
-    id,
-    varos: request.body.varos,
-    negyed: request.body.negyed,
-    terulet: Number(request.body.terulet),
-    ar: Number(request.body.ar),
-    szoba: Number(request.body.szoba),
-    kepURL: null,
+    varos: sanitize(String(request.body.varos)),
+    negyed: sanitize(String(request.body.negyed)),
+    terulet: sanitize(Number(request.body.terulet)),
+    ar: sanitize(Number(request.body.ar)),
+    szoba: sanitize(Number(request.body.szoba)),
+    felhasznaloNev: sanitize(request.body.felhasznaloNev),
   };
 
   if (ujLakas.varos.trim() === '' || ujLakas.negyed.trim() === '') {
-    response.set('Content-Type', 'text/plain; charset=utf-8');
-    response.status(400).send('Hiba: A város és negyed mezők nem lehetnek üresek!');
+    response.render('ujlakas', { hiba: 'A mezők nem lehetnek üresek!' });
     return;
   }
 
   if (isNaN(ujLakas.terulet) || isNaN(ujLakas.ar) || isNaN(ujLakas.szoba)) {
-    response.set('Content-Type', 'text/plain; charset=utf-8');
-    response.status(400).send('Hiba: A felszínterület, ár és szobák száma mezőknek kötelező értéket adni!');
+    console.error(`ERROR NUM: 400`);
+    response.render('fooldal', { hiba: 'Hiba: A felszínterület, ár és szobák száma mezőknek kötelező értéket adni!' });
     return;
   }
 
   if (ujLakas.terulet <= 0 || ujLakas.ar <= 0 || ujLakas.szoba <= 0) {
-    response.set('Content-Type', 'text/plain; charset=utf-8');
-    response.status(400).send('Hiba: A felszínterület, ár és szobák száma mezőknek pozitív értéket kell adni!');
+    console.error(`ERROR NUM: 400`);
+    response.render('fooldal', {
+      hiba: 'Hiba: A felszínterület, ár és szobák száma mezőknek kötelező pozitív értéket adni!',
+    });
     return;
   }
 
   if (ujLakas.szoba % 1 !== 0) {
-    response.set('Content-Type', 'text/plain; charset=utf-8');
-    response.status(400).send('Hiba: A szobák számának egész számnak kell lennie!');
+    console.error(`ERROR NUM: 400`);
+    response.render('fooldal', {
+      hiba: 'Hiba: A szobák számának egész számnak kell lennie!',
+    });
     return;
   }
 
   const uzenet = `A szerver sikeresen megkapta az információkat!:
+  Felhasználónév: ${request.body.felhasznaloNev}
   Lakás (város): ${request.body.varos}
   Lakás (negyed): ${request.body.negyed}
   Felszínterület: ${request.body.terulet}
   Ár: ${request.body.ar}
   Szobák száma: ${request.body.szoba}`;
   console.log(uzenet);
-  await db.insert(ujLakas);
-
-  response.set('Content-Type', 'text/plain; charset=utf-8');
-  response.send(`Sikeres adatküldés!\nID: ${ujLakas.id}\n${uzenet}`);
-  id++;
+  await insertIntoDatabase(ujLakas);
+  const talalatok = await executeQuery({});
+  response.render('fooldal', { talalatok });
 });
 
-function megfelel(lakas, minAr, maxAr, minSzoba, maxSzoba, varos, negyed) {
-  return (
-    lakas.ar >= minAr &&
-    lakas.ar <= maxAr &&
-    lakas.szoba >= minSzoba &&
-    lakas.szoba <= maxSzoba &&
-    lakas.varos.toLowerCase().includes(varos) &&
-    lakas.negyed.toLowerCase().includes(negyed)
-  );
-}
-
 app.get('/kereses', async (request, response) => {
+  const felhasznaloNev = String(request.query.felhasznaloNev);
   const minAr = Number(request.query.min_ar) || 0;
   const maxAr = Number(request.query.max_ar) || Infinity;
   const minSzoba = Number(request.query.min_szoba) || 0;
   const maxSzoba = Number(request.query.max_szoba) || Infinity;
   const varos = request.query.varos ? request.query.varos.toString().toLowerCase() : '';
   const negyed = request.query.negyed ? request.query.negyed.toString().toLowerCase() : '';
-
-  const lakasok = await db.find({});
-  const talalatok = lakasok.filter((lakas) => megfelel(lakas, minAr, maxAr, minSzoba, maxSzoba, varos, negyed));
-  response.set('Content-Type', 'application/json; charset=utf-8');
-  response.send(JSON.stringify(talalatok));
+  const talalatok = await executeQuery({
+    felhasznaloNev: { $regex: felhasznaloNev, $options: 'i' },
+    ar: { $gte: minAr, $lte: maxAr },
+    szoba: { $gte: minSzoba, $lte: maxSzoba },
+    varos: { $regex: varos, $options: 'i' },
+    negyed: { $regex: negyed, $options: 'i' },
+  });
+  response.render('fooldal', { talalatok });
 });
+
+app.get('/reszletes/:id', async (request, response) => {
+  const lakasId = sanitize(request.params.id);
+  try {
+    const lakasArray = await executeQuery({ _id: new ObjectId(lakasId) });
+    if (lakasArray.length === 0) {
+      console.error(`ERROR NUM: 404`);
+      response.render('reszletes', { hiba: 'Hiba: Nem talállható lakás a megadott ID-val!' });
+      return;
+    }
+    const lakas = lakasArray[0];
+    response.render('reszletes', { lakas });
+  } catch (err) {
+    console.error(`ERROR NUM: 500 ${err}`);
+    response.render('reszletes', { hiba: 'Hiba: Az oldalt nem sikerült betölteni!' });
+  }
+});
+
 app.listen(3000, () => {
   console.log('Szerver fut a 3000-es porton');
 });
